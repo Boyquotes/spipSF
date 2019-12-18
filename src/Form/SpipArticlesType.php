fa79ffbce45a244be0bc9c3b2b75c05f0b95d277
boyquotes <webmaster@linux-live-cd.org>
2019-12-18 17:45:22 +0000

    Articles admib

---
+++ src/Form/SpipArticlesType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\SpipArticles;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SpipArticlesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('idParent')
+            ->add('titre')
+            ->add('descriptif')
+            ->add('texte')
+            ->add('idSecteur')
+            ->add('statut')
+            ->add('lang')
+            ->add('langueChoisie')
+            ->add('statutTmp')
+            ->add('profondeur')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => SpipArticles::class,
+        ]);
+    }
+}
